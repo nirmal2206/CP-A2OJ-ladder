6548d1d6aea6906a7ae245d833699550610d0748
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-04-03 08:27:10 +0530

    Added A_Young_Physicist

---
+++ A_Young_Physicist.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define nl "\n"
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int x = 0, y = 0, z = 0;
+    while (n--)
+    {
+        int i, j, k;
+        cin >> i >> j >> k;
+        x += i;
+        y += j;
+        z += k;
+    }
+    if (x == 0 && y == 0 && z == 0)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+    solve();
+    // }
+    return 0;
+}
