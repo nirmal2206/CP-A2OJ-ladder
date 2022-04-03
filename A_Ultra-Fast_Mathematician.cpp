fa4a38e564f6304bfcb25683837fc71e2987f8fc
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-04-03 16:50:19 +0530

    Added A_Ultra-Fast_Mathematician

---
+++ A_Ultra-Fast_Mathematician.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define nl "\n"
+
+void solve()
+{
+    string s1, s2, ans = "";
+    cin >> s1 >> s2;
+    for (int i = 0; i < s1.size(); ++i)
+    {
+        if (s1[i] == s2[i])
+        {
+            ans += "0";
+        }
+        else
+        {
+            ans += "1";
+        }
+    }
+    cout << ans << nl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+    solve();
+    // }
+    return 0;
+}
