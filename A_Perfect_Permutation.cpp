ee8a29a42d9c1cab21652b6960cf7aa26b5eb2ab
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-06-07 17:46:20 +0530

    Added A_Perfect_Permutation

---
+++ A_Perfect_Permutation.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define nl "\n"
+
+void solve()
+{
+    int n;
+    cin >> n;
+    if (n % 2)
+    {
+        cout << -1 << nl;
+        return;
+    }
+    for (int i = 1; i <= n; i += 2)
+    {
+        cout << i + 1 << " " << i << " ";
+    }
+    cout << nl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+    solve();
+    // }
+    return 0;
+}
