e7de48b57752cd1d0790b16d9b250bb993492c0e
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-06-08 19:38:31 +0530

    Added A_Soft_Drinking

---
+++ A_Soft_Drinking.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define nl "\n"
+
+void solve()
+{
+    int n, k, l, c, d, p, nll, np;
+    cin >> n >> k >> l >> c >> d >> p >> nll >> np;
+    int drinks_possible = (k * l) / nll;
+    int lime_slices = c * d;
+    int salt = p / np;
+    int ans = min(drinks_possible, min(lime_slices, salt)) / n;
+    cout << ans;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+    solve();
+    // }
+    return 0;
+}
