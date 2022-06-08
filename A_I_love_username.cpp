7a901911d6a490d7c2601fa2dee2e4fde31cc44e
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-06-08 10:48:36 +0530

    Added A_I_love_username

--- A_I_love_username.cpp
+++ A_I_love_username.cpp
@@ -1,11 +1,10 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
 typedef vector<int> vi;
-typedef pair<int,int> pii;
-
+typedef pair<int, int> pii;
 
 #define pb push_back
 #define mp make_pair
@@ -13,44 +12,48 @@ typedef pair<int,int> pii;
 #define S second
 #define nl "\n"
 
-void solve(){
+void solve()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int x;
-    cin>>x;
-    int curr_min=x,curr_max=x, ans=0;
-    for(int i=1;i<n;++i){
-        cin>>x;
-        if(x>curr_max){
-            curr_max=x;
+    cin >> x;
+    int curr_min = x, curr_max = x, ans = 0;
+    for (int i = 1; i < n; ++i)
+    {
+        cin >> x;
+        if (x > curr_max)
+        {
+            curr_max = x;
             ans++;
         }
-        if(x<curr_min){
-            curr_min=x;
+        if (x < curr_min)
+        {
+            curr_min = x;
             ans++;
         }
     }
-    cout<<ans;
+    cout << ans;
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    #ifndef ONLINE_JUDGE
- 
+#ifndef ONLINE_JUDGE
+
     // For getting input from input.txt file
     freopen("input.txt", "r", stdin);
- 
+
     // Printing the Output to output.txt file
     freopen("output.txt", "w", stdout);
- 
-    #endif
+
+#endif
     // ll t;
     // cin >> t;
     // while (t--)
     // {
-        solve();
+    solve();
     // }
     return 0;
 }
