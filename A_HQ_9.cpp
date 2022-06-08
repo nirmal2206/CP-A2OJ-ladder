3a769f0414e6fd9ee4f9939d86cec61bc94851a8
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-06-08 19:44:55 +0530

    Added A_HQ_9

---
+++ A_HQ_9.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define nl "\n"
+
+void solve()
+{
+    string s;
+    cin >> s;
+    for (char c : s)
+    {
+        if (c == 'H' || c == 'Q' || c == '9')
+        {
+            cout << "YES";
+            return;
+        }
+    }
+    cout << "NO";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+    solve();
+    // }
+    return 0;
+}
