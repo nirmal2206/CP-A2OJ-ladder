d8c6f42bc297ed14ef42e04e51599955465e5ada
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-06-08 10:45:36 +0530

    Added A_I_love_username

---
+++ A_I_love_username.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define nl "\n"
+
+void solve(){
+    int n;
+    cin>>n;
+    int x;
+    cin>>x;
+    int curr_min=x,curr_max=x, ans=0;
+    for(int i=1;i<n;++i){
+        cin>>x;
+        if(x>curr_max){
+            curr_max=x;
+            ans++;
+        }
+        if(x<curr_min){
+            curr_min=x;
+            ans++;
+        }
+    }
+    cout<<ans;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+ 
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+ 
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+ 
+    #endif
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+        solve();
+    // }
+    return 0;
+}
