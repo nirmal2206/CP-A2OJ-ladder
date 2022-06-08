eddde29a4fdd60eedc0e1b4958f78e00623b7eaf
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-06-08 17:00:58 +0530

    Added A_Is_your_horseshoe_on_the_other_hoof

---
+++ A_Is_your_horseshoe_on_the_other_hoof.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define nl "\n"
+
+void solve()
+{
+    set<int> s;
+    for (int i = 0; i < 4; ++i)
+    {
+        int x;
+        cin >> x;
+        s.insert(x);
+    }
+    int ans = 4 - s.size();
+    cout << ans;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+    solve();
+    // }
+    return 0;
+}
