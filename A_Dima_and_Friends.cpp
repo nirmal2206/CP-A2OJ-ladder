7dc543297cbc7548f300c5df294a5d542911a081
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-06-10 19:03:32 +0530

    Added A_Dima_and_Friends

--- A_Dima_and_Friends.cpp
+++ A_Dima_and_Friends.cpp
@@ -1,11 +1,10 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
 typedef vector<int> vi;
-typedef pair<int,int> pii;
-
+typedef pair<int, int> pii;
 
 #define pb push_back
 #define mp make_pair
@@ -13,40 +12,44 @@ typedef pair<int,int> pii;
 #define S second
 #define nl "\n"
 
-void solve(){
+void solve()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int sum = 0;
-    for(int i=0;i<n;++i){
+    for (int i = 0; i < n; ++i)
+    {
         int x;
-        cin>>x;
-        sum+=x;
+        cin >> x;
+        sum += x;
     }
-    int ans=0;
-    for(int i=1;i<=5;++i){
-        if((sum+i)%(n+1)!=1) ans++;
+    int ans = 0;
+    for (int i = 1; i <= 5; ++i)
+    {
+        if ((sum + i) % (n + 1) != 1)
+            ans++;
     }
-    cout<<ans;
+    cout << ans;
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    #ifndef ONLINE_JUDGE
- 
+#ifndef ONLINE_JUDGE
+
     // For getting input from input.txt file
     freopen("input.txt", "r", stdin);
- 
+
     // Printing the Output to output.txt file
     freopen("output.txt", "w", stdout);
- 
-    #endif
+
+#endif
     // ll t;
     // cin >> t;
     // while (t--)
     // {
-        solve();
+    solve();
     // }
     return 0;
 }
