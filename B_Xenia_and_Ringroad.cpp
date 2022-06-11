3c107fb2e94dbbf1d88bb00357ef5398e016b070
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-06-11 20:11:14 +0530

    Added B_Xenia_and_Ringroad

---
+++ B_Xenia_and_Ringroad.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define nl "\n"
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    int prev = 1, curr;
+    ll time = 0;
+    while (m--)
+    {
+        cin >> curr;
+        if (curr < prev)
+        {
+            time += n - prev + curr;
+        }
+        else
+        {
+            time += curr - prev;
+        }
+        prev = curr;
+    }
+    cout << time;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+    solve();
+    // }
+    return 0;
+}
