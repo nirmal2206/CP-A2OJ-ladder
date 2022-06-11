cbd0b35edf8b1f04c8d416d67b4a4470c405e7d0
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-06-11 10:16:26 +0530

    Added A_Petr_and_Book

---
+++ A_Petr_and_Book.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define nl "\n"
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi v(7, 0);
+    for (int &it : v)
+    {
+        cin >> it;
+    }
+    int i = 0;
+    while (i < 7)
+    {
+        n -= v[i];
+        if (n <= 0)
+            break;
+        i++;
+        i = i % 7;
+    }
+    cout << i + 1;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+    solve();
+    // }
+    return 0;
+}
