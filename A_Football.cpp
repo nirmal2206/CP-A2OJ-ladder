60e3a78c01bbaea6a5e0634cc830f180a2789798
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-06-11 20:48:45 +0530

    Added A_Football

---
+++ A_Football.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define nl "\n"
+
+void solve()
+{
+    int n;
+    cin >> n;
+    unordered_map<string, int> mp;
+    while (n--)
+    {
+        string s;
+        cin >> s;
+        mp[s]++;
+    }
+    int max = -1;
+    string winner = "";
+    for (auto it : mp)
+    {
+        if (it.S > max)
+        {
+            max = it.S;
+            winner = it.F;
+        }
+    }
+    cout << winner;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+    solve();
+    // }
+    return 0;
+}
