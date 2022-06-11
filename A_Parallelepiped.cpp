090432eec294dda6519406d4a90bee33675e5799
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-06-11 11:15:30 +0530

    Added A_Parallelepiped

---
+++ A_Parallelepiped.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define nl "\n"
+
+void solve()
+{
+    int ab, bc, ac;
+    cin >> ab >> bc >> ac;
+    int a = sqrt((ab * ac) / bc);
+    int b = sqrt((ab * bc) / ac);
+    int c = sqrt((bc * ac) / ab);
+    int sum = 4 * (a + b + c);
+    cout << sum;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+    solve();
+    // }
+    return 0;
+}
