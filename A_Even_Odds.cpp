76187dcbe9c6871dc51a8dc86f5761aa1be2c17e
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-06-11 17:06:00 +0530

    Added A_Even_Odds

---
+++ A_Even_Odds.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define nl "\n"
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    ll half = ceil(n / 2.0);
+    if (k <= half)
+        cout << (k * 2) - 1;
+    else
+        cout << (k - half) * 2;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+    solve();
+    // }
+    return 0;
+}
