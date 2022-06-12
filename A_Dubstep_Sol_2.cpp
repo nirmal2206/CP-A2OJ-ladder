1e2bab7789f43be344a9051a6fc46118e22f5528
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-06-12 09:51:43 +0530

    Added A_Dubstep_Sol_2

---
+++ A_Dubstep_Sol_2.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define nl "\n"
+
+void solve()
+{
+    string s;
+    cin >> s;
+    int n = s.size();
+    bool flag = true;
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (s[i] == 'W' && s[i + 1] == 'U' && s[i + 2] == 'B')
+        {
+            i += 2;
+            if (!flag)
+            {
+                cout << " ";
+                flag = true;
+            }
+        }
+        else
+        {
+            flag = false;
+            cout << s[i];
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+    solve();
+    // }
+    return 0;
+}
