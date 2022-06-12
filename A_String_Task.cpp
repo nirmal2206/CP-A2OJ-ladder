fd48a7b2ed7db79fe7f886ddf841d736d37f08a0
nirmal2206 <nirmalpanda22061998@gmail.com>
2022-06-12 17:04:41 +0530

    Added A_String_Task

---
+++ A_String_Task.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define nl "\n"
+
+void solve()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); ++i)
+    {
+        s[i] = tolower(s[i]);
+        if (s[i] != 'a' && s[i] != 'o' && s[i] != 'y' && s[i] != 'e' && s[i] != 'u' && s[i] != 'i')
+            cout << "." << s[i];
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    // {
+    solve();
+    // }
+    return 0;
+}
